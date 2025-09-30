84205c6aad2a993591747929cc170c5ed3f1020e
Muhammadirfan08 <irfan020807@gmail.com>
2025-09-30 10:34:52 +0700

    PROJECT

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::create([
+            'nama'     => 'Irfan IT',
+            'email'    => 'admin@gmail.com',
+            'jabatan'  => 'Admin',
+            'password' => Hash::make('123123123'),
+            'is_tugas' => false,
+        ]);
+
+        User::create([
+            'nama'     => 'Fadel',
+            'email'    => 'fadel@gmail.com',
+            'jabatan'  => 'Karyawan',
+            'password' => Hash::make('123123123'),
+            'is_tugas' => false,
+        ]);
+
+        User::create([
+            'nama'     => 'bobon',
+            'email'    => 'bobon@gmail.com',
+            'jabatan'  => 'Karyawan',
+            'password' => Hash::make('123123123'),
+            'is_tugas' => false,
+        ]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Authcontroller;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TugasController;
+use App\Http\Controllers\DashboardController;
+
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('welcome');
+
+// Login
+Route::get('login', [AuthController::class, 'login'])->name('login');
+Route::post('login', [AuthController::class, 'loginProses'])->name('loginProses');
+
+// logout
+Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::middleware('checkLogin')->group(function () {
+    // Dasboard
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    // User
+    Route::get('user', [UserController::class, 'index'])->name('user');
+    Route::get('user/create', [UserController::class, 'create'])->name('userCreate');
+    Route::post('user/store', [UserController::class, 'store'])->name('userStore');
+    Route::get('user/edit/{id}', [UserController::class, 'edit'])->name('userEdit');
+    Route::post('user/update/{id}', [UserController::class, 'update'])->name('userUpdate');
+    Route::delete('user/destroy/{id}', [UserController::class, 'destroy'])->name('userDestroy');
+    Route::get('user/excel', [UserController::class, 'excel'])->name('userExcel');
+
+
+    // Tugas
+    Route::get('tugas', [TugasController::class, 'index'])->name('tugas');
+});
