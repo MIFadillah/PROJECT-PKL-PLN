9d66f4e6d1cc5600c696f8704e2e2043d3eb2107
Muhammadirfan08 <irfan020807@gmail.com>
2025-10-07 11:10:28 +0700

    Menampilkan Jumlah Karyawan Yang Ditugaskan

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,11 +10,12 @@ class DashboardController extends Controller
     public function index()
     {
         $data = array(
-            'title'          => "Dashboard",
-            'menuDashboard'  => "active",
-            "jumlahUser"     => User::count(),
-            "jumlahAdmin"    => User::where('jabatan', 'Admin')->count(),
-            "jumlahKaryawan"    => User::where('jabatan', 'Karyawan')->count(),
+            'title'               => "Dashboard",
+            'menuDashboard'       => "active",
+            "jumlahUser"          => User::count(),
+            "jumlahAdmin"         => User::where('jabatan', 'Admin')->count(),
+            "jumlahKaryawan"      => User::where('jabatan', 'Karyawan')->count(),
+            "jumlahDitugaskan"    => User::where('jabatan', 'Karyawan')->where('is_tugas', True)->count(),
         );
 
         return view('dashboard', $data);
