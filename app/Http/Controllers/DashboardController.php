889f237e899913be4a56b125487830ab7d84ff7a
Muhammadirfan08 <irfan020807@gmail.com>
2025-10-08 10:16:14 +0700

    Menampilkan Jumlah Karyawan Yang Belum Ditugaskan

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,12 +10,13 @@ class DashboardController extends Controller
     public function index()
     {
         $data = array(
-            'title'               => "Dashboard",
-            'menuDashboard'       => "active",
-            "jumlahUser"          => User::count(),
-            "jumlahAdmin"         => User::where('jabatan', 'Admin')->count(),
-            "jumlahKaryawan"      => User::where('jabatan', 'Karyawan')->count(),
-            "jumlahDitugaskan"    => User::where('jabatan', 'Karyawan')->where('is_tugas', True)->count(),
+            'title'                    => "Dashboard",
+            'menuDashboard'            => "active",
+            "jumlahUser"               => User::count(),
+            "jumlahAdmin"              => User::where('jabatan', 'Admin')->count(),
+            "jumlahKaryawan"           => User::where('jabatan', 'Karyawan')->count(),
+            "jumlahDitugaskan"         => User::where('jabatan', 'Karyawan')->where('is_tugas', true)->count(),
+            "jumlahBelumDitugaskan"    => User::where('jabatan', 'Karyawan')->where('is_tugas', false)->count(),
         );
 
         return view('dashboard', $data);
